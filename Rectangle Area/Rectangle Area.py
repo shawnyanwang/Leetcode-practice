1fb951b8958f3cd376d23f27e143d8d14dbcda3c
shawnyanwang <shawnyanwang@gmail.com>
2015-06-26 17:20:41 -0700

    add rectangle area

---
+++ Rectangle Area/Rectangle Area.py
@@ -0,0 +1,24 @@
+__author__ = 'shawn.wang'
+
+#
+# Find the total area covered by two rectilinear rectangles in a 2D plane.
+#
+# Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+#
+# Rectangle Area
+
+
+class Solution:
+    # @param {integer} A
+    # @param {integer} B
+    # @param {integer} C
+    # @param {integer} D
+    # @param {integer} E
+    # @param {integer} F
+    # @param {integer} G
+    # @param {integer} H
+    # @return {integer}
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        abs(G-A)*abs(D-F)
+
+
